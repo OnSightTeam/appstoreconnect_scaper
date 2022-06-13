0b35a8a45fa0828d28cfb6f67f8f2b0cb42f7ac2
Nikolay Chaban <nchaban.kh@gmail.com>
2022-06-13 20:12:24 +0300

    2fa
    - deleted webdriver, used it from webdriver manager
    - added ability fill 2fa code

--- chrome_scraper.py
+++ chrome_scraper.py
@@ -2,9 +2,11 @@ import os
 import time
 
 from dotenv import load_dotenv
+from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -20,7 +22,7 @@ chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--window-size=1920x1080")
 
 ## run web driver and assign to the driver variable
-with webdriver.Chrome(f'{os.getcwd()}/chromedriver', options=chrome_options) as driver:
+with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options) as driver:
     driver.get(link)
     wait = WebDriverWait(driver, 10)
 
@@ -33,9 +35,10 @@ with webdriver.Chrome(f'{os.getcwd()}/chromedriver', options=chrome_options) as 
         ## find email text form and fill with account
         account_field = wait.until(EC.element_to_be_clickable((By.ID, "account_name_text_field")))
         account_field.send_keys(os.getenv('APPLE_ACCOUNT'))
+        time.sleep(2)
         account_field.send_keys(Keys.ENTER)
 
-        ## activate remind me checkmark to avoid next enter login process
+        ## activate remind me checkbox to avoid next enter login process
         remember_me = wait.until(EC.element_to_be_clickable((By.ID, "remember-me-label")))
         remember_me.click()
 
@@ -43,8 +46,33 @@ with webdriver.Chrome(f'{os.getcwd()}/chromedriver', options=chrome_options) as 
         password_field.send_keys(os.getenv('APPLE_PWD'))
         password_field.send_keys(Keys.ENTER)
 
+        is_security_code_container = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "security-code")))
+
+        if is_security_code_container:
+            code = input()
+
+            for n, item in enumerate(code, start=0):
+                id = 'char' + str(n)
+                code_entry = wait.until(EC.element_to_be_clickable((By.ID, id)))
+                code_entry.send_keys(item)
+
+            trust_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@class="button-bar"]//button[3]')))
+            trust_btn.click()
+
+            driver.get('https://appstoreconnect.apple.com/apps/1572271000/appstore/activity/ios/ratingsResponses?m=')
+
+            ##TODO: -Need to fix this code first-
+            # reviews_wait = WebDriverWait(driver, 20)
+
+            # reviews_container = reviews_wait.until(EC.visibility_of_element_located((By.ID, 'reviews-container')))
+            # if reviews_container:
+            #     print(driver.page_source)
+            # else:
+            #     print('Page content is:')
+            #     print(driver.page_source)
+
     ## timeout just to make sure page loads correct and browser didn't closed at the end of the method immediately
-    time.sleep(5)
+    time.sleep(15)
 
     ## close driver - memory management
     driver.quit()
