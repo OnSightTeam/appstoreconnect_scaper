9586b8536c5b9fd514fdd377bebab90babe90274
Nikolay Chaban <nchaban.kh@gmail.com>
2022-06-15 19:24:14 +0300

    - added loading loaded page cookies and history - added logic for storing page cookies if user made login - load cookies of the page if it exists

--- chrome_scraper.py
+++ chrome_scraper.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import time
 
 from dotenv import load_dotenv
@@ -20,16 +21,24 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("user-data-dir=appstoreconnect")
 
 ## run web driver and assign to the driver variable
 with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options) as driver:
     driver.get(link)
     wait = WebDriverWait(driver, 10)
 
-    ## make sure page redirects to the auth form
-    is_login_form_visible = wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID,"aid-auth-widget-iFrame")))
+    try:
+        cookies = pickle.load(open("cookies.pkl", "rb"))
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+    except:
+        print('For website: ', link, 'no cookies')
 
-    if is_login_form_visible:
+    ## make sure page redirects to the auth form
+    try:
+        is_login_form_visible = wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID,"aid-auth-widget-iFrame")))
+
         load_dotenv()
 
         ## find email text form and fill with account
@@ -59,17 +68,25 @@ with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=
             trust_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@class="button-bar"]//button[3]')))
             trust_btn.click()
 
-            driver.get('https://appstoreconnect.apple.com/apps/1572271000/appstore/activity/ios/ratingsResponses?m=')
+            try:
+                cookies = driver.get_cookies()
+                pickle.dump(cookies, open("cookies.pkl","wb"))
+            except:
+                print('Error with load and storing cookies')
+    except:
+        print("User already logged in")
 
-            ##TODO: -Need to fix this code first-
-            # reviews_wait = WebDriverWait(driver, 20)
+    driver.get('https://appstoreconnect.apple.com/apps/1572271000/appstore/activity/ios/ratingsResponses?m=')
 
-            # reviews_container = reviews_wait.until(EC.visibility_of_element_located((By.ID, 'reviews-container')))
-            # if reviews_container:
-            #     print(driver.page_source)
-            # else:
-            #     print('Page content is:')
-            #     print(driver.page_source)
+    ##TODO: -Need to fix this code first-
+    # reviews_wait = WebDriverWait(driver, 20)
+
+    # reviews_container = reviews_wait.until(EC.visibility_of_element_located((By.ID, 'reviews-container')))
+    # if reviews_container:
+    #     print(driver.page_source)
+    # else:
+    #     print('Page content is:')
+    #     print(driver.page_source)
 
     ## timeout just to make sure page loads correct and browser didn't closed at the end of the method immediately
     time.sleep(15)
