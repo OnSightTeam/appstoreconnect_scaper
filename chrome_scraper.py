df8f893854eebbdcf018a5bc0ba2f9a995cb11cd
Alex <lebeichuk.sasha@gmail.com>
2022-06-17 19:38:39 +0300

    -implemented changing region, scroll to bottom of infinity scroll, parsing review

--- chrome_scraper.py
+++ chrome_scraper.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import time
+import pandas as pd
 
 from dotenv import load_dotenv
 from webdriver_manager.chrome import ChromeDriverManager
@@ -43,27 +44,29 @@ with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=
 
         ## find email text form and fill with account
         account_field = wait.until(EC.element_to_be_clickable((By.ID, "account_name_text_field")))
-        account_field.send_keys(os.getenv('APPLE_ACCOUNT'))
-        account_field.send_keys(Keys.ENTER)
+        if not account_field.get_attribute('value'):
+            account_field.send_keys(os.getenv('APPLE_ACCOUNT'))
+            account_field.send_keys(Keys.ENTER)
 
         ## activate remind me checkbox to avoid next enter login process
+        #TODO:add check if it's clicked
         remember_me = wait.until(EC.element_to_be_clickable((By.ID, "remember-me-label")))
         remember_me.click()
 
         password_field = wait.until(EC.element_to_be_clickable((By.ID, "password_text_field")))
-        password_field.send_keys(os.getenv('APPLE_PWD'))
-        password_field.send_keys(Keys.ENTER)
+        if not password_field.get_attribute('value'):
+            password_field.send_keys(os.getenv('APPLE_PWD'))
+            password_field.send_keys(Keys.ENTER)
 
         is_security_code_container = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "security-code")))
 
         if is_security_code_container:
             code = input()
-
             for n, item in enumerate(code, start=0):
                 id = 'char' + str(n)
                 code_entry = wait.until(EC.element_to_be_clickable((By.ID, id)))
                 code_entry.send_keys(item)
-
+            time.sleep(10)
             trust_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@class="button-bar"]//button[3]')))
             trust_btn.click()
 
@@ -77,17 +80,40 @@ with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=
 
     driver.get('https://appstoreconnect.apple.com/apps/1572271000/appstore/activity/ios/ratingsResponses?m=')
 
-    ##TODO: -Need to fix this code first-
+    time.sleep(15)
 
-    try:
+    is_page_content_container = wait.until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, "//iframe[@title='legacy']")))
+    if is_page_content_container:
+        time.sleep(5)
+
+        choose_region = wait.until(EC.element_to_be_clickable((By.ID, "store-fronts-selector-menu")))
+        choose_region.click()
+
+        all_regions_option = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "All Countries or Regions")))
+        all_regions_option.click()
+        time.sleep(2)
+
+        #scroll to bottom of reviews
         reviews_container = wait.until(EC.visibility_of_element_located((By.ID, 'reviews-container')))
-        print(driver.page_source)
-    except:
-        print('Page content is:')
-        print(driver.page_source)
+        driver.execute_script("arguments[0].scrollIntoView(true);", reviews_container)
 
-    ## timeout just to make sure page loads correct and browser didn't closed at the end of the method immediately
-    time.sleep(25)
+        keys = ["title", "author", "review_message"]
+        df = pd.DataFrame(columns=keys)
 
-    ## close driver - memory management
-    driver.quit()
+        try:
+            reviews = driver.find_elements(By.CLASS_NAME, "review-container")
+            # iterate through list and get text
+            for review in reviews[:-1]:
+                review_data = review.text.split("\n")
+                review_data.pop(1)
+                review_data.pop(3)
+                a_series = pd.Series(review_data, index = df.columns)
+                df = df.append(a_series, ignore_index=True)
+            print(df)
+        except Exception as e:
+            print(e)
+            print(type(e))
+
+        #timeout just to make sure page loads correct and browser didn't closed at the end of the method immediately
+        time.sleep(10)
+        driver.quit()
